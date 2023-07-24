07528920195a6345fa2759256871906b5f3fa1d2
patel7vishal <pv251023@gmail.com>
2023-07-24 12:55:54 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as contractABI from './abi.json'
+import { AbiType } from './abiType';
+import { ethers } from 'ethers'
+import { Abi__factory } from './types/ethers-contracts/factories/Abi__factory'
+
 
 function App() {
+  // Ethereum wallet connection
+  const infuraUrl = "https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID";
+  const provider = new ethers.JsonRpcProvider(infuraUrl);
+  const contractAddress = "0xSMART_CONTRACT_ADDRESS";
+  const contract = Abi__factory.connect(contractAddress, provider)
+  // contract.transfer(); //got type
+
+  const getTotalSupply = async () => {
+    const totalSupply = await contract.totalSupply(); //totalSupply also get the type
+    return totalSupply;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button>
+        HI
+      </button>
     </div>
   );
 }
